b3a42d9be39b6abeefe5a0cea22c6c6860a09138
FedeFGR <fedefgr@gmail.com>
2023-04-14 12:32:32 -0300

    muchos cambios

--- js/cargarAutos.js
+++ js/cargarAutos.js
@@ -1,14 +1,16 @@
-/*------------
-fetch("../BD/Autos.json")
-.then(response => {
-   return response.json();
-})
-.then(data => console.log(data));
 
-*/
 let Autos
+let Seleccionado
 
 const CatalogoContainer=document.getElementById("CatalogoContainer");
+const Container=document.getElementById("AutoContainer");
+
+function AbrirAuto(){
+   localStorage.setItem("auto",JSON.stringify(Seleccionado))
+   window.location.href='auto.html'
+}
+
+
 function Filtrar(){
    const Marca=document.getElementById("marca").value
    const Color=document.getElementById("color").value
@@ -17,7 +19,7 @@ function Filtrar(){
       if(auto.Marca==Marca && auto.Color==Color || Marca=="Todas" && auto.Color==Color ||
       auto.Marca==Marca && Color=="Cualquiera" || Marca=="Todas" && Color=="Cualquiera"){
          CatalogoContainer.innerHTML+=`
-         <div class="card">
+         <div class="card" onclick="Seleccionado=auto; AbrirAuto();">
             <img class="imgcard" src="${auto.URL}">
             <div class="cardescripcion">
                <h4><b>${auto.Marca} ${auto.Modelo}</b></h4>
--- js/validar.js
+++ js/validar.js
@@ -26,8 +26,63 @@ function Validar(){
 		msg=msg+"\nEl E-mail es incorrecto";
 	if(msg=="")
 	{	alert("Datos enviados correctamente\nGracias por suscribirse! ");
-        //document.getElementById("suscripcion").action="contacto.html";
-	   	return true;
+     	return true;
+	}
+	else
+	{	alert("Datos incorrectos: \n " + msg);
+		return false;
+	}
+}
+function Validar2(){
+    let cadena=document.getElementById("nombre2").value;
+	let filtroEmail= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	let msg="";
+			
+	//verificar Nombre
+	if(cadena.length==0 || cadena.length>50)
+		msg=msg+"\nEl Nombre está vacío o tiene mas de 50 letras";
+	for(i=0;i<cadena.length;i++)
+		if(!(cadena[i]>='a' && cadena[i]<='z' || cadena[i]>='A' && cadena[i]<='Z'))
+		{	msg=msg+"\nEl Nombre debe tener solo letras";
+			break;
+		}
+	//verificar email
+	cadena=document.getElementById("email2").value;
+	if (!filtroEmail.test(cadena))
+		msg=msg+"\nEl E-mail es incorrecto";
+	if(msg=="")
+	{	alert("Datos enviados correctamente\nNos pondremos en contacto con usted para brindarle la información necesaria para adquirir el vehiculo seleccioando ");
+     	return true;
+	}
+	else
+	{	alert("Datos incorrectos: \n " + msg);
+		return false;
+	}
+}
+
+function Validar3(){
+    let cadena=document.getElementById("nombre3").value;
+	let filtroEmail= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	let msg="";
+			
+	//verificar Nombre
+	if(cadena.length==0 || cadena.length>50)
+		msg=msg+"\nEl Nombre está vacío o tiene mas de 50 letras";
+	for(i=0;i<cadena.length;i++)
+		if(!(cadena[i]>='a' && cadena[i]<='z' || cadena[i]>='A' && cadena[i]<='Z'))
+		{	msg=msg+"\nEl Nombre debe tener solo letras";
+			break;
+		}
+	//verificar email
+	cadena=document.getElementById("email3").value;
+	if (!filtroEmail.test(cadena))
+		msg=msg+"\nEl E-mail es incorrecto";
+	cadena=document.getElementById("message").value;
+	if(cadena=="")
+		msg=msg+"\nEl Mensaje de la consulta no puede estar vacío";
+	if(msg=="")
+	{	alert("Datos enviados correctamente\nNos pondremos en contacto con usted para intentar solucionar su consulta ");
+     	return true;
 	}
 	else
 	{	alert("Datos incorrectos: \n " + msg);
